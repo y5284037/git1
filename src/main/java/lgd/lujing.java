957fcfaa5a12c52b16a065de0abc7a13a958ff33
lujing <452604407@qq.com>
2018-03-16 19:13:17 +0800

    sout

--- src/main/java/lgd/lujing.java
+++ src/main/java/lgd/lujing.java
@@ -1,9 +1,13 @@
 package lgd;
 
+import com.sun.media.sound.SoftTuning;
+
 /**
  * @author lujing
  * Create_at 2018/3/16 19:10
  */
 public class lujing {
-
+    public static void main(String[] args) {
+        System.out.println("heoo");
+    }
 }
